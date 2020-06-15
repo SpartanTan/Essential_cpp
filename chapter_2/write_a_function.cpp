bcee1639270ce2ac8e6db8f7e41d3773f659763d
Zhicun Tan <tanzc9866@126.com>
2020-06-15 14:08:31 +0800

    add func print_sequence() in write_a_function.cpp

--- chapter_2/write_a_function.cpp
+++ chapter_2/write_a_function.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 
-//前置z声明(forward declaration)
+//前置声明(forward declaration)
 //让编译器知道这个函数的存在
 bool fibon_elem( int , int & );
+bool print_sequence( int );
 
 int main()
 {
@@ -18,6 +19,7 @@ int main()
 	else
 		cout << "Sorry. Could not calculate element # "
 			 << pos << endl;
+	print_sequence( pos );
 }
 
 bool fibon_elem( int pos, int &elem)
@@ -35,13 +37,48 @@ bool fibon_elem( int pos, int &elem)
 	{
 		elem = n_2 + n_1;
 		n_1 = n_2;
-		n_2 = elem;
+		n_2 = elem;//这二者的赋值顺序不能搞错
 		//cout << n_1
 		//	 << n_2 << endl;
 	}
 	return true;
 }
 
+bool print_sequence( int pos )
+{
+	if( pos <= 0 || pos > 1024 )
+	{
+		cerr << "invalid position: " << pos
+			 << " -- cannot handle request!\n";
+		return false;
+	}
+	cout << "The Fibonacci Sequence for "
+		 << pos << " positions: \n\t";
 
+	//所有位置都会打印出1 1，只有pos == 1除外
+	switch( pos )
+	{
+		default:
+		case 2:
+			cout << "1 ";
+			//注意，此处没有break
+		case 1:
+			cout << "1 ";
+			break;
+	}
 
+	int elem;
+	int n_1 = 1, n_2 = 1;
+	for( int ix = 3; ix <= pos; ++ix )
+	{
+		elem = n_1 + n_2;
+		n_1 = n_2;
+		n_2 = elem;
+		//一行打印10个元素
+		cout << elem << ( !( ix % 10 ) ? "\n\t" : " " );
+	}
+	cout << endl;
+
+	return true;
+}
 
